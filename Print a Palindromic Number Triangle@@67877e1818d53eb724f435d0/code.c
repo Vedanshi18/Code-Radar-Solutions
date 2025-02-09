7e2b6ff44b9fa5f7b8591f08b6ed42802979a2a7
Vedanshi18 <vedanshi.1886@gmail.com>
2025-02-09 18:00:55 +0530

    Solution to Print a Palindromic Number Triangle

--- Print a Palindromic Number Triangle@@67877e1818d53eb724f435d0/code.c
+++ Print a Palindromic Number Triangle@@67877e1818d53eb724f435d0/code.c
@@ -1,21 +1,40 @@
+// #include <stdio.h>
+// int main() {
+//     int n,i,j,k;
+//     scanf("%d",&n);
+//      for (i=1; i<=n; i++){
+//         k=0;
+//         for (j=1; j<=n-i; j++){
+//             printf(" ");
+//         }
+//         for (j=1; j<=i; j++){
+//             k++;
+//             printf("%d",k);
+//         } 
+//         for (j=1; j<=i-1; j++){
+//             k--;
+//             printf("%d",k);
+//         }  
+//         printf("\n"); 
+//      }
+//     return 0;
+// }
+
 #include <stdio.h>
 int main() {
-    int n,i,j,k;
-    scanf("%d",&n);
-     for (i=1; i<=n; i++){
-        k=0;
-        for (j=1; j<=n-i; j++){
+    int n, i, j;
+    scanf("%d", &n);
+    for (i=1; i<=n; i++) {
+        for (j=1; j<=n-i; j++) {
             printf(" ");
         }
-        for (j=1; j<=i; j++){
-            k++;
-            printf("%d",k);
-        } 
-        for (j=1; j<=i-1; j++){
-            k--;
-            printf("%d",k);
-        }  
-        printf("\n"); 
-     }
+        for (j = 1; j <= i; j++) {
+            printf("%d", j);
+        }
+        for (j = i - 1; j >= 1; j--) {
+            printf("%d", j);
+        }
+        printf("\n");
+    }
     return 0;
 }
