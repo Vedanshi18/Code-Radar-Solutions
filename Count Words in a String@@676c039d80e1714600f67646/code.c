735d27a09eb2a161cfef399c4723c1f3abc8119e
Vedanshi <vedanshi.1886@gmail.com>
2025-02-25 23:23:37 +0530

    Solution to Count Words in a String

--- Count Words in a String@@676c039d80e1714600f67646/code.c
+++ Count Words in a String@@676c039d80e1714600f67646/code.c
@@ -1,14 +1,33 @@
-#include<stdio.h>
-#include<string.h>
-int main(){
+#include <stdio.h>
+#include <string.h>
+
+int main() {
     char s[100];
-    fgets(s,sizeof(s),stdin);
-    int l=strlen(s);
-    int c=0;
-    for (int i=0; s[i]!='\0';i++){
-        if (s[i]==' ' && s[i+1]!=' ' && s[i+1]!='\0') {
+    int c = 0;
+
+    printf("Enter a string: ");
+    fgets(s, sizeof(s), stdin);
+    s[strcspn(s, "\n")] = '\0'; // Remove newline character
+
+    int i = 0;
+
+    // Skip leading spaces
+    while (s[i] == ' ') {
+        i++;
+    }
+
+    // Count words separated by spaces
+    for (; s[i] != '\0'; i++) {
+        if (s[i] == ' ' && s[i + 1] != ' ' && s[i + 1] != '\0') {
             c++;
         }
     }
-    printf("%d",c+1);
+
+    // If the string is not empty, count the first word
+    if (strlen(s) > 0 && s[0] != '\0' && s[0] != ' ') {
+        c++;
+    }
+
+    printf("Number of words: %d\n", c);
+    return 0;
 }
