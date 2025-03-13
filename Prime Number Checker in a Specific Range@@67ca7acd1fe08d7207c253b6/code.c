bef20f34b2ebc357bb93a50451aab4f662056ec7
Vedanshi <vedanshi.1886@gmail.com>
2025-03-13 19:56:39 +0530

    Solution to Prime Number Checker in a Specific Range

--- Prime Number Checker in a Specific Range@@67ca7acd1fe08d7207c253b6/code.c
+++ Prime Number Checker in a Specific Range@@67ca7acd1fe08d7207c253b6/code.c
@@ -1,28 +1,42 @@
-// int isPrime(int num){
-//     if (num==0 || num==1) return 0;
-//     if (num==2 || num==3) return 1;
-//     if(num%2==0) return 0;
-//     for (int i=3; i<=sqrt(num); i=i+2){
-//         if (num%i==0) return 0;
-//     }
-// }
-void printPrimesInRange(int a,int b){
-    int p=0;
-    for (int i=a; i<=b; i++){
-        int c=0;
-        for (int j=1; j<=i; j++){
-            if(i%j==0) c++;
-        }
-        if(c==2) {
+int isPrime(int num){
+    if (num==0 || num==1) return 0;
+    if (num==2 || num==3) return 1;
+    if(num%2==0) return 0;
+    for (int i=3; i<=sqrt(num); i=i+2){
+        if (num%i==0) return 0;
+    }
+}
+void printPrimesInRange(int a, int b){
+    int found=0;
+    for (int i=a; i<=b: i++){
+        if(isPrime(i)==1){
             printf("%d ",i);
-            p=1;
+            found=1;
         }
     }
-    if (p==0){
+    if(found==0){
         printf("No prime numbers");
     }
 }
-// OPTIMAL & EFFICIENT
+
+// //My Method
+// void printPrimesInRange(int a,int b){
+//     int p=0;
+//     for (int i=a; i<=b; i++){
+//         int c=0;
+//         for (int j=1; j<=i; j++){
+//             if(i%j==0) c++;
+//         }
+//         if(c==2) {
+//             printf("%d ",i);
+//             p=1;
+//         }
+//     }
+//     if (p==0){
+//         printf("No prime numbers");
+//     }
+// }
+// // OPTIMAL & EFFICIENT
 // int isPrime(int num) {
 //     if (num < 2) return 0;  // 0 and 1 are not prime
 //     if (num == 2 || num == 3) return 1;  // 2 and 3 are prime
