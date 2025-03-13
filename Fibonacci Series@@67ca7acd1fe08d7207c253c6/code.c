c4a5f3d3b4d658b3d823fcfd0910d03b9d149c73
Vedanshi <vedanshi.1886@gmail.com>
2025-03-13 20:11:15 +0530

    Solution to Fibonacci Series

---
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -0,0 +1,12 @@
+void fibonacciSeries(int n){
+    for (int i=1; i<=n: i++){
+        printf(nTerm(n));
+    }
+}
+int nTerm(int n){
+    if(n==0) return 0;
+    if(n==1) return 1;
+    for (int i=1; i<=n-2; i++){
+        return nTerm(n-1)+nTerm(n-2);
+    }
+}
